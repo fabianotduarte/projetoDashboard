c9f8791c50b9dacb5ec28d0930ec83f8c95f9550
professorisidro <isidro@professorisidro.com.br>
2020-06-29 12:00:09 -0300

    Adicionando Cross Origin

--- src/main/java/br/com/isidrocorp/projeto/controller/DepartamentoController.java
+++ src/main/java/br/com/isidrocorp/projeto/controller/DepartamentoController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import br.com.isidrocorp.projeto.dao.DepartamentoDAO;
 import br.com.isidrocorp.projeto.model.Departamento;
 
 @RestController
+@CrossOrigin("*") 
 public class DepartamentoController {
 
 	@Autowired
@@ -34,7 +36,4 @@ public class DepartamentoController {
 			return ResponseEntity.notFound().build();
 		}
 	}
-	
-	
-	
 }
